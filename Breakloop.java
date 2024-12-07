1d96248bdd4ee6dd0cb7d04fa205b2eca059bd16
munawar900munawar <shaikskarishma9@gmail.com>
2024-12-07 12:44:34 +0530

    Add files via upload

---
+++ Breakloop.java
@@ -0,0 +1,9 @@
+class Breakloop{
+    public static void main(String args[]){
+        for(int i=0;i<5;i++){
+            if(i==2)
+            break;
+        System.out.println(i);
+        }
+    }
+}
