4e3b8fb5bee92bf9b999dd49729669d9b5363eea
munawar900munawar <shaikskarishma9@gmail.com>
2024-12-07 12:48:07 +0530

    Add files via upload

---
+++ continueloop.java
@@ -0,0 +1,9 @@
+class continueloop{
+    public static void main(String args[]){
+        for(int i=0;i<5;i++){
+            if(i==2)
+            continue;
+        System.out.println(i);
+        }
+    }
+}
